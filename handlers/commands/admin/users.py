2c71691b3b992889b87fa09722f91f30e03ae24f
Atageldiev <ulukmanatageldiuulu@gmail.com>
2020-07-24 23:23:55 +0600

    make it better

--- handlers/commands/admin/users.py
+++ handlers/commands/admin/users.py
@@ -6,9 +6,6 @@ from data.config import ADMIN_ID
 
 @dp.message_handler(lambda message: message.from_user.id == ADMIN_ID, commands="users", commands_prefix="!")
 async def users(message: Message):
-    users = []
-    for el in db.get_user_ids():
-        user_id = el[0]
-        users.append(user_id)
+    users = db.get_user_ids():
     await ChatActions.typing()
     await bot.send_message(chat_id=ADMIN_ID, text=f"Батя, бот ща насчитывает:---   <b>{len(users)}</b>   ---пользователей")
