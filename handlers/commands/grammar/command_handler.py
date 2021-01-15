0af4a6fec11f903a761006d5de234c665d04ca3c
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-15 14:58:08 +0600

    Class based command handlers

--- handlers/commands/grammar/command_handler.py
+++ handlers/commands/grammar/command_handler.py
@@ -1,17 +1,23 @@
-from aiogram.dispatcher.filters.builtin import Command
 from aiogram.types import Message
 
-from core.conf import dp
+from library.handlers.class_based import CommandHandler
 from utils.database import db
 from utils.decorators import typing_action, check_user_existance
 from .utils import get_markup_by_key_from_json
 
 
-@dp.message_handler(Command("grammar"))
-@typing_action
-@check_user_existance
-async def grammar(message: Message):
-    db.grammar_used += 1
+class GrammarHandler(CommandHandler):
+    decorators = [typing_action, check_user_existance]
 
-    markup = await get_markup_by_key_from_json("default")
-    await message.answer("Выберите насчет чего вы хотите получить готовую информацию: ", reply_markup=markup)
+    async def handle(self, message: Message):
+        db.grammar_used += 1
+
+        markup = await get_markup_by_key_from_json("default")
+        await message.answer("Выберите насчет чего вы хотите получить готовую информацию: ", reply_markup=markup)
+
+
+class FuckYouBitchHandler(CommandHandler):
+    decorators = [typing_action]
+
+    async def handle(self, message: Message):
+        await message.answer("GO FUCK YOURSELF YO BITCH!")
