83d3894ff691c51072002171d1344f1ed8622b3b
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-19 15:33:47 +0600

    Even more code refactoring and solving parser tasksk

--- utils/parser.py
+++ utils/parser.py
@@ -7,63 +7,27 @@ from utils.buttons import get_ikb
 HEADERS = {'user-agent': 'my-app/0.0.1'}
 
 
-def __get_text(text):
+def __get_text(text: str) -> str:
     """Returns a text without any extra spaces anywhere"""
     return " ".join(text.split())
 
 
-def __get_html(data):
-    src = data["src"]
-    dest = data["dest"]
-    word = data["word"]
-
-    url = f"https://context.reverso.net/перевод/{src}-{dest}/{word}"
+def __get_html(data: dict) -> BeautifulSoup:
+    url = f"https://context.reverso.net/перевод/{data['src']}-{data['dest']}/{data['word']}"
     response = requests.get(url, headers=HEADERS)
-    html = BeautifulSoup(response.content, "html.parser")
-    return html
+    return BeautifulSoup(response.content, "html.parser")
 
 
 def parse_examples(data, num):
     """Parses only examples from context.reverso.net"""
-    html = __get_html(data)
-
-    if html.select('.example'):
+    html = __get_html(data).select(".example")
+    if html:
+        html = html[num - 2:num + 1]
         msg = "Вот примеры\n"
-        html = html.select('.example')[num - 2:num + 1]
         for el in html:
             msg += "---" + __get_text(el.select('.src')[0].text) + "\n"
             msg += "---" + __get_text(el.select('.trg')[0].text) + "\n\n"
-
         return msg, get_ikb({"text": "Еще примеры", "callback_data": "more_examples"})
-    else:
-        return "ERROR, TRY AGAIN", None
-
-
-def parse_translations(data):
-    """
-    Parses only translations from context.reverso.net
-    Creates buttons and sends them directly to the user:
-        Text on buttons - translation of the word
-        URL on buttons - link to context.reverso.net where examples that are only with the requested word
-        Max number of buttons - 6
-    """
-    html = __get_html(data)
-    word = data["word"]
-    msg = f"Вот переводы слова {word}\n" \
-          f"Нажмите на соответствующую кнопку, чтобы перейти на сайт и увидеть примеры только с этим словом:"
-
-    if html.select('.translation.ltr'):
-        markup = InlineKeyboardMarkup(row_width=3, inline_keyboard=True)
-
-        for el in html.select('.translation.ltr')[:6]:
-            text = __get_text(el.text)
-            url = "https://context.reverso.net" + str(el.get('href'))
-
-            markup.insert(InlineKeyboardButton(text=text, url=url))
-
-        markup.add(InlineKeyboardButton(text="Показать примеры", callback_data="show_examples"))
-
-        return msg, markup
     else:
         return "ERROR, TRY AGAIN", None
 
@@ -71,19 +35,14 @@ def parse_translations(data):
 async def parse_native_english(message, url):
     """Parses native-english.ru, and gets links to the websites about requested rule of english"""
     response = requests.get(url, headers=HEADERS)
-    html = BeautifulSoup(response.content, "html.parser")
-
-    html = html.select('.list__item > a')
+    html = BeautifulSoup(response.content, "html.parser").select('.list__item > a')[:-3]
     if html:
-        del html[-3:]
         markup = InlineKeyboardMarkup()
         markup.add(InlineKeyboardButton(text="Определение | Definition", url=url))
 
         for el in html:
             url = "https://www.native-english.ru" + str(el.get('href'))
-            text = __get_text(el.text)
-
-            markup.add(InlineKeyboardButton(text=text, url=url))
+            markup.add(InlineKeyboardButton(text=__get_text(el.text), url=url))
 
         await message.answer(
             "Вот что я нашел:\nНажмите на соответствующую кнопку, чтобы перейти на сайт и почитать подробнее",
