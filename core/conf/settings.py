7a0447147c481e476339da42b2d757728e11db46
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-01-20 14:51:01 +0600

    Dotenv update

--- core/conf/settings.py
+++ core/conf/settings.py
@@ -11,7 +11,6 @@ if DEBUG:
 else:
     TOKEN = "1368801526:AAFoG3C84uthcDinURZ031e2WOt9fcUzTjM"
 
-load_dotenv()
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=MemoryStorage())
 storage = dp.storage
@@ -24,6 +23,7 @@ ADMINS = [
 # Path
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, ".env.dev"))
 # Database
 DATABASE = {
     "user": os.getenv("DB_USER"),
