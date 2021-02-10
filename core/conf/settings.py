6dae21369b5ed60f5509f8550f246efef94f47e8
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-02-10 13:27:03 +0600

    Remove private info

--- core/conf/settings.py
+++ core/conf/settings.py
@@ -5,11 +5,12 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from dotenv import load_dotenv
 
-DEBUG = True
-if DEBUG:
-    TOKEN = "1397404758:AAEBfHXgaxWM0j-FGnsFsUxQjo4sMuyEz5Q"
-else:
-    TOKEN = "1368801526:AAFoG3C84uthcDinURZ031e2WOt9fcUzTjM"
+# Path
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env.dev"))
+
+TOKEN = os.getenv("TOKEN")
 
 bot = Bot(token=TOKEN, parse_mode="HTML")
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -20,10 +21,6 @@ ADMINS = [
     399344900
 ]
 
-# Path
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-load_dotenv(os.path.join(BASE_DIR, ".env.dev"))
 # Database
 DATABASE = {
     "user": os.getenv("DB_USER"),
