623a6dc2d13870cad71f14794ffb714cd97cea00
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-05-04 10:06:26 +0600

    Update

--- utils/formatters.py
+++ utils/formatters.py
@@ -3,31 +3,6 @@ UNDERLINED = "<u>{}</u>"
 CURSIVE = "<em>{}</em>"
 
 
-def bold(text):
-    """Returns bold text"""
-    return BOLD.format(text)
-
-
-def underlined(text):
-    """Returns underlined text"""
-    return UNDERLINED.format(text)
-
-
-def bold_underlined(text):
-    """Returns text that is bold and underlined at the same time"""
-    return bold(underlined(text))
-
-
-def cursive(text):
-    """Returns cursive text"""
-    return CURSIVE.format(text)
-
-
-def strip_text(text: str) -> str:
-    """Returns a text without any extra spaces anywhere"""
-    return " ".join(text.split())
-
-
 class FormattedText:
     def __init__(self, text):
         self.text = text
