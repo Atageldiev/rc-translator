bac3d695676eb7a2c46d90ab9293b2b54f632008
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-05-04 09:50:21 +0600

    Update

--- utils/path.py
+++ utils/path.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def get_current_dir(file):
     """
     Returns path to the current directory
-    Usage: get_current_dir(__file__)
+    Example:
+        >>> current_dir = get_current_dir(__file__)
     """
     return Path(file).resolve().parent
